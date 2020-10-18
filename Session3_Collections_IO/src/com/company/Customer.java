755d30b5d268a13f5ac7ebd7f9279f6182aa8ae9
saptarshi <saptarshi.p@flipkart.com>
2020-10-18 13:43:01 +0530

    Added  java session 3

---
+++ Session3_Collections_IO/src/com/company/Customer.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Customer {
+    Integer id;
+    String name;
+    Integer rating;
+
+    public Customer(Integer id, String name, Integer rating) {
+        this.id = id;
+        this.name = name;
+        this.rating = rating;
+    }
+}
